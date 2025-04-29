b52ce124e62b27761bd9af9c84945d5e213dd13e
Michael Rosendahl <michael.rosendahl@ltu.se>
2025-04-29 15:45:55 +0200

    Removed icon since it can be added in calendars

--- generateCalendars.js
+++ generateCalendars.js
@@ -124,7 +124,7 @@ METHOD:PUBLISH
 
         const hours = days[day] || 0; // Default to 0 if no work hours are specified
         const date = formatDate(year, monthIndex + 1, day);
-        const eventSummary = `🕒 ${summary.replace('${hours}', hours)}`; // Add watch icon to the summary
+        const eventSummary = `${summary.replace('${hours}', hours)}`; // Add watch icon to the summary
         const eventDescription = description
           .replace('${day}', day)
           .replace('${monthName}', monthName)
